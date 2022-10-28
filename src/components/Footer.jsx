608399bf5effda12fefb2dc86d7db2633cda12c8
WeeNeeBee <weeneeadams@gmail.com>
2022-10-28 16:41:57 +0100

    added footer images alt attribute

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -16,9 +16,9 @@ const Footer = () => {
             <hr />
             {/* icons for Zuri and I4G */}
             <div className="zuri-hng-ig4-icons">
-                <a href="https://internship.zuri.team/"> <img src={Zuri} /> </a>
+                <a href="https://internship.zuri.team/"> <img src={Zuri} alt = 'zuri-logo'/> </a>
                 <p>HNG Internship 9 Frontend Task</p>
-                <a href="https://ingressive.org/"> <img src={I4G} /> </a>
+                <a href="https://ingressive.org/"> <img src={I4G} alt = 'i4g-logo'/> </a>
             </div>
         </div>
     )
