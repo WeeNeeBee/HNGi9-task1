699170b42d4cf922fdd0b50fa7490f43884930bc
WeeNeeBee <weeneeadams@gmail.com>
2022-11-04 05:00:17 +0100

    refactor: Adjusted App to accomodate Contact page.

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import Profile from "./components/Profile";
 import Links from "./components/Links";
 import Footer from "./components/Footer";
-import { RiShareForwardLine } from 'react-icons/ri'
-import { BiDotsHorizontalRounded } from 'react-icons/bi'
+import AppComps from "./AppComps";
+import { Route, Routes } from "react-router-dom";
+import Contact from './pages/Contact'
 import './App.css'
 
 
 
 
 const App = () => {
-  const buttons = [
-    {id: 'btn__zuri', text: 'Zuri Team', link:'https://training.zuri.team'},
-    {id: 'books', text: 'Zuri Books', link:'http://books.zuri.team/', subText:'Hurray!!! This is your lucky day, you have just landed where you will find the best learner-friendly books on design and coding.'},
-    {id: 'book__python', text: 'Python Books', link:'https://books.zuri.team/python-for-beginners?ref_id=<WeeNeeBee>', subText:'Get value for a python beginners book. Become a Python expert quicker than you imagined!'},
-    {id: 'pitch', text: 'Background Check for Coders', link:'https://background.zuri.team/', subText:'We offer proffesional coders scout services. Click to jump in!'},
-    {id: 'book__design', text: 'Design Books', link:'https://books.zuri.team/design-rules', subText:'You have just hit the jackpot! Click for a free copy of the Zuri design book for beginners.'}
-]
   return (
     <div className="App">
-      <RiShareForwardLine className="share-icon" title="share"/>
-      <BiDotsHorizontalRounded className="mobile-menu" />
-      <Profile />
-      <Links buttons = {buttons}/>
+      <Routes>
+        <Route path="/" element = {<AppComps />} />
+        <Route path="/contact" element = {<Contact />}/>
+      </Routes>
       <Footer />
     </div>
   );
---
+++ src/pages/Contact.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const Contact = () => {
+    return(
+        <h1>Testing...</h1>
+    )
+}
+
+
+
+export default Contact
