1df9f411eed7899e59f33db614b85f60b6d5e792
WeeNeeBee <weeneeadams@gmail.com>
2022-11-04 12:29:52 +0100

    fix: Fixed react-router-dom bugs.

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Profile from "./components/Profile";
 import Links from "./components/Links";
 import Footer from "./components/Footer";
 import AppComps from "./AppComps";
-import { Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Contact from './pages/Contact'
 import './App.css'
 
@@ -12,10 +12,12 @@ import './App.css'
 const App = () => {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element = {<AppComps />} />
-        <Route path="/contact" element = {<Contact />}/>
-      </Routes>
+      <Router>
+        <Routes>
+          <Route path="/" element = {<AppComps />} />
+          <Route path="/contact" element = {<Contact />}/>
+        </Routes>
+      </Router>
       <Footer />
     </div>
   );
