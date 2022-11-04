33c3079df5c14a55a9508e47c644027df8628cb1
WeeNeeBee <weeneeadams@gmail.com>
2022-11-04 22:32:16 +0100

    style: Adjusted form container padding (affecting footer).

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,11 @@
-import React from "react";
 import './contact.css'
 
 
 const Contact = () => {
+    
+    
+    const myName = "WeeNeeBee";
 
-    const name = "WeeNeeBee";
     return(
         <div className="container">
             <h1>Contact Me</h1>
@@ -13,29 +14,29 @@ const Contact = () => {
                 <div className="first-last-name">
                     <div className="form-control">
                         <label>First name</label>
-                        <input type="text" id="first_name" placeholder="Enter your first name" />
+                        <input type="text" id="first_name" placeholder="Enter your first name"  />
                         
                     </div>
 
                     <div className="form-control">
                         <label>Last name</label>
-                        <input type="text" id="last_name" placeholder="Enter your last name" />
+                        <input type="text" id="last_name" placeholder="Enter your last name"  />
                     </div>
                 </div>
 
                 <div className="form-control">
                     <label>Email</label>
-                    <input type="enail" id="email" placeholder="yourname@gmail.com" />
+                    <input type="enail" id="email" placeholder="yourname@gmail.com"  />
                 </div>
 
                 <div className="form-control">
                     <label>Message</label>
-                    <textarea id="message" placeholder = "Send me a message and I'll reply you as soon as possible..."></textarea>
+                    <textarea id="message" placeholder = "Send me a message and I'll reply you as soon as possible..."  ></textarea>
                 </div>
 
                 <div className="checkbox">
-                    <input type= 'checkbox' />
-                    <label>You agree to providing your data to {name} who may contact you.</label>
+                    <input type= 'checkbox'  />
+                    <label>You agree to providing your data to {myName} who may contact you.</label>
                 </div>
 
                 <button id="btn__submit" className="btn">Send Message</button>
