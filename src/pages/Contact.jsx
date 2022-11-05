3752db0fea4ef1b52a51b72d9f5ed7817b1a4bcd
WeeNeeBee <88585151+WeeNeeBee@users.noreply.github.com>
2022-11-05 21:19:18 +0100

    fix: My name (agreement checkbox)

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -24,7 +24,7 @@ const Contact = () => {
     })
     const [isSubmit, setIsSubmit] = useState(false)
     
-    const myName = "WeeNeeBee";
+    const myName = "Winnifred Ngwalang";
 
     const handleChange = (e) => {
         const { name, value } = e.target;
